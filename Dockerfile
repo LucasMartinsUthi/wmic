84f6d22a5303d9fb8c4b7ca46ffcabbed9fa44bc
LucasMartinsUthi <luscagbr@gmail.com>
2022-08-08 16:14:10 -0300

    docker centos

--- Dockerfile
+++ Dockerfile
@@ -8,7 +8,7 @@ RUN yum -y install \
     curl \
     gcc
 
-RUN curl https://sh.rustup.rs -sSf | sh -s -- -y
+RUN curl https://sh.rustup.rs -sSf | sh -s -- -y 
 
 # Add .cargo/bin to PATH
 ENV PATH="/root/.cargo/bin:${PATH}"
--- src/main.rs
+++ src/main.rs
@@ -5,52 +5,13 @@ use serde::{Serialize, Deserialize};
 use tokio::net::TcpStream;
 
 use std::env;
-use std::fmt;
-use std::fmt::write;
 use std::process;
-use std::io::{self, Write};
 
 use snafu::prelude::*;
 
 use std::collections::HashMap;
-use wmi::Variant;
 
-
-use wmic::{Args};
-
-#[derive(Serialize, Deserialize, Debug)]
-struct MonstaVariant(Variant);
-
-impl fmt::Display for MonstaVariant {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        
-        match self.0 {
-            Variant::Empty => write!(f, "Empty"),
-            Variant::Null => write!(f, "Null"),
-            Variant::String(ref s) => write!(f, "{}", s),
-            Variant::I1(i) => write!(f, "{}", i),
-            Variant::I2(i) => write!(f, "{}", i),
-            Variant::I4(i) => write!(f, "{}", i),
-            Variant::I8(i) => write!(f, "{}", i),
-            Variant::R4(i) => write!(f, "{}", i),
-            Variant::R8(i) => write!(f, "{}", i),
-            Variant::Bool(b) => write!(f, "{}", b),
-            Variant::UI1(i) => write!(f, "{}", i),
-            Variant::UI2(i) => write!(f, "{}", i),
-            Variant::UI4(i) => write!(f, "{}", i),
-            Variant::UI8(i) => write!(f, "{}", i),
-            Variant::Array(ref v) => {
-                write!(f, "[")?;
-                // for i in v {
-                //     // let monsta_variant = MonstaVariant(i);
-                //     // write!(f, "{}", monsta_variant)?;
-                // }
-                write!(f, "]")
-            }
-
-        }
-    }
-}
+use wmic::Args;
 
 #[derive(Serialize, Deserialize, Debug)]
 struct QueryWMI {
@@ -66,7 +27,9 @@ enum QueryResultStatus {
 
 #[derive(Serialize, Deserialize, Debug)]
 struct QueryResult {
-    items: Vec<HashMap<String, MonstaVariant>>,
+    
+    items: Vec<HashMap<String, String>>,
+
     status: QueryResultStatus,
     message: String,
 }
@@ -79,8 +42,8 @@ pub enum Error {
 	Serde { source: serde_json::Error },
 	#[snafu(display("No private keys in key file"))]
 	NoPrivateKeys,
-    #[snafu(display("Error addrs"))]
-	ErrorAddrs,
+    #[snafu(display("Error ErrorWinproxy"))]
+	ErrorWinproxy
 }
 
 type Result<T> = std::result::Result<T, Error>;
@@ -94,12 +57,12 @@ async fn main() -> Result<()> {
         process::exit(1);
     });
 
-    let mut stream = TcpStream::connect("127.0.0.1:7743").await.context(IoSnafu)?;
+    let mut stream = TcpStream::connect(format!("{}:7743", args.address)).await.context(IoSnafu)?;
 
     let query = QueryWMI {
-        // wql: args.wql,
-        wql: "select * from Win32_ComputerSystem".to_string(),
-        password: "secret".to_string(),
+        // wql: "select * from Win32_ComputerSystem".to_string(),
+        wql: args.wql,
+        password: String::new(),
     };
 
     let query = serde_json::to_string(&query).context(SerdeSnafu)?;
@@ -117,24 +80,28 @@ async fn main() -> Result<()> {
     let mut response = String::new();
     reader.read_to_string(&mut response).await.context(IoSnafu)?;
     
-    // response to json
-    // println!("{}", response);
+    // response to QueryResult
     let response: QueryResult  = serde_json::from_str(&response).context(SerdeSnafu)?;
-    
-    //get item keys from first item
-    let mut keys = Vec::new();
-    for (key, _) in response.items[0].iter() {
-        keys.push(key.to_string());
-    }
-    println!("{}", keys.join("|"));
 
-
-    // loop over items and print there values
-    for item in response.items {
-        for value in item.values() {
-            print!("{}|", value);
+    match response.status {
+        QueryResultStatus::Success => {},
+        QueryResultStatus::Failure => {
+            println!("{}", response.message);
+            return Err(Error::ErrorWinproxy);
         }
     }
+
+    let mut keys = Vec::new();
+    let mut values = Vec::new();
+
+    for (key, value) in response.items[0].iter() {
+        keys.push(key.to_string());
+        values.push(value.to_string());
+        
+    }
+
+    println!("{}", keys.join("|"));
+    println!("{}", values.join("|"));
 
     Ok(())
 }
